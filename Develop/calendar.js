33516d35436f1b2df8bd368dcd96dc7c90b6a93c
Ian Erickson <ierickson25@gmail.com>
2020-01-28 02:18:50 -0600

    initial commit

---
+++ Develop/calendar.js
@@ -0,0 +1,13 @@
+//Save Events for each hour of the day
+//Display standard business hours (9am - 5pm)
+//Each time slot should represent 1 hour and contain
+//  The Time
+//  A field to hold user input
+//  A save button
+//      Stores time and user input in  'localstorage'
+//Top of Cal shoud display current date and time
+//Each hour should be color coded to reflect if time slot is in the past, present, or future
+//
+
+console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
+console.log(moment().format("dddd"));
